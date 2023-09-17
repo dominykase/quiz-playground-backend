7644b5ea7c86e5635fa652b45ae15f52016da5df
Dominykas <dominykas.eicinas@gmail.com>
2023-09-17 12:50:39 +0300

    added category weights population upon category creation
    feeling weird today :?

--- src/main/java/com/quizplayground/quizplayground/useCases/category/CreateCategoryUseCase.java
+++ src/main/java/com/quizplayground/quizplayground/useCases/category/CreateCategoryUseCase.java
@@ -1,13 +1,16 @@
 package com.quizplayground.quizplayground.useCases.category;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.quizplayground.quizplayground.exceptions.QuizNotFoundException;
+import com.quizplayground.quizplayground.models.Category;
+import com.quizplayground.quizplayground.models.CategoryWeight;
 import com.quizplayground.quizplayground.models.Quiz;
+import com.quizplayground.quizplayground.repositories.AnswerRepository;
 import com.quizplayground.quizplayground.repositories.CategoryRepository;
+import com.quizplayground.quizplayground.repositories.CategoryWeightRepository;
 import com.quizplayground.quizplayground.repositories.QuizRepository;
 import com.quizplayground.quizplayground.requestDtos.category.PostCategoryRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCategoryUseCase {
@@ -15,10 +18,32 @@ public class CreateCategoryUseCase {
   private CategoryRepository categoryRepository;
   @Autowired
   private QuizRepository quizRepository;
+  @Autowired
+  private CategoryWeightRepository categoryWeightRepository;
+  @Autowired
+  private AnswerRepository answerRepository;
 
   public Quiz handle(Long quizId, PostCategoryRequestDto requestDto) {
     Quiz quiz = this.quizRepository.findById(quizId).orElseThrow(() -> new QuizNotFoundException(quizId));
-    this.categoryRepository.save(requestDto.toCategory(quiz));
+    Category category = this.categoryRepository.save(requestDto.toCategory(quiz));
+
+    quiz.getQuestions()
+        .forEach(
+            (question) -> {
+              question
+                  .getAnswers()
+                  .forEach(
+                      (answer) -> {
+                        CategoryWeight categoryWeight = new CategoryWeight();
+                        categoryWeight.setAnswer(answer);
+                        categoryWeight.setCategory(category);
+                        categoryWeight.setWeight(1);
+
+                        this.categoryWeightRepository.save(categoryWeight);
+                        this.answerRepository.refresh(answer);
+                      });
+            });
+
     this.quizRepository.refresh(quiz);
 
     return quiz;
